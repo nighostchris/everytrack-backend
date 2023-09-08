d4afd88ba55cac3f1878fc96ee7037db21d25189
Chris <chrisliupascal@gmail.com>
2023-09-08 19:34:54 +0800

    Create asset_provider, asset_provider_account_type and account tables

---
+++ migrations/000004_create_asset_provider_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS everytrack_backend.asset_provider (
+  id          UUID          DEFAULT gen_random_uuid() PRIMARY KEY,
+  name        TEXT          UNIQUE NOT NULL,
+  icon        TEXT          NOT NULL,
+  created_at  TIMESTAMPTZ   NOT NULL DEFAULT now(),
+  updated_at  TIMESTAMPTZ   NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER asset_provider_updated_at
+BEFORE UPDATE ON everytrack_backend.asset_provider
+FOR EACH ROW
+EXECUTE PROCEDURE on_update_timestamp();

b054d9dadad9878176e0d809750c4a61e11f8c48
Chris <chrisliupascal@gmail.com>
2023-09-14 21:14:16 +0800

    Introduce country, stock and account_stock table as prepation for Stock Management feature + Write seed files for new tables above

--- seeds/00001_init_asset_provider.ts
+++ seeds/00001_init_asset_provider.ts
@@ -4,23 +4,35 @@ const table = "asset_provider";
 const schema = "everytrack_backend";
 
 export async function seed(knex: Knex): Promise<void> {
-  await knex(`${schema}.${table}`).del();
-  await knex(`${schema}.${table}`).insert([
-    { name: "Chase Bank (UK)", icon: "/chase_bank_uk.svg", type: "savings" },
-    { name: "HSBC (HK)", icon: "/hsbc_hk.svg", type: "savings" },
-    { name: "HSBC (UK)", icon: "/hsbc_uk.svg", type: "savings" },
-    { name: "Bank of China (HK)", icon: "/boc_hk.svg", type: "savings" },
-    { name: "Lloyds Bank (UK)", icon: "/lloyds_uk.svg", type: "savings" },
-    { name: "Citibank (HK)", icon: "/citibank_hk.svg", type: "savings" },
-    { name: "Hang Seng Bank (HK)", icon: "/hang_seng_hk.svg", type: "savings" },
-    { name: "Metro Bank (UK)", icon: "/metro_bank_uk.svg", type: "savings" },
-    { name: "Monzo (UK)", icon: "/monzo_uk.svg", type: "savings" },
-    { name: "Nationwide (UK)", icon: "/nationwide_uk.svg", type: "savings" },
-    {
-      name: "Standard Chartered (HK)",
-      icon: "/standard_chartered_hk.svg",
-      type: "savings",
-    },
-    { name: "Starling (UK)", icon: "/starling_uk.svg", type: "savings" },
-  ]);
+  const haveRecords = await knex(`${schema}.${table}`)
+    .count("id", {
+      as: "rows",
+    })
+    .first();
+
+  if ((haveRecords && Number(haveRecords.rows) > 0) === false) {
+    await knex(`${schema}.${table}`).del();
+    await knex(`${schema}.${table}`).insert([
+      { name: "Chase Bank (UK)", icon: "/chase_bank_uk.svg", type: "savings" },
+      { name: "HSBC (HK)", icon: "/hsbc_hk.svg", type: "savings" },
+      { name: "HSBC (UK)", icon: "/hsbc_uk.svg", type: "savings" },
+      { name: "Bank of China (HK)", icon: "/boc_hk.svg", type: "savings" },
+      { name: "Lloyds Bank (UK)", icon: "/lloyds_uk.svg", type: "savings" },
+      { name: "Citibank (HK)", icon: "/citibank_hk.svg", type: "savings" },
+      {
+        name: "Hang Seng Bank (HK)",
+        icon: "/hang_seng_hk.svg",
+        type: "savings",
+      },
+      { name: "Metro Bank (UK)", icon: "/metro_bank_uk.svg", type: "savings" },
+      { name: "Monzo (UK)", icon: "/monzo_uk.svg", type: "savings" },
+      { name: "Nationwide (UK)", icon: "/nationwide_uk.svg", type: "savings" },
+      {
+        name: "Standard Chartered (HK)",
+        icon: "/standard_chartered_hk.svg",
+        type: "savings",
+      },
+      { name: "Starling (UK)", icon: "/starling_uk.svg", type: "savings" },
+    ]);
+  }
 }
--- seeds/00002_init_asset_provider_account_type.ts
+++ seeds/00002_init_asset_provider_account_type.ts
@@ -4,196 +4,204 @@ const schema = "everytrack_backend";
 const table = "asset_provider_account_type";
 
 export async function seed(knex: Knex): Promise<void> {
-  await knex(`${schema}.${table}`).del();
+  const haveRecords = await knex(`${schema}.${table}`)
+    .count("id", {
+      as: "rows",
+    })
+    .first();
 
-  const assetProviders: { id: string; name: string }[] = await knex(
-    `${schema}.asset_provider`
-  ).select("id", "name");
-  const assetProvidersMap: any = {};
-  assetProviders.forEach((assetProvider) => {
-    assetProvidersMap[assetProvider.name] = assetProvider.id;
-  });
+  if ((haveRecords && Number(haveRecords.rows) > 0) === false) {
+    await knex(`${schema}.${table}`).del();
 
-  await knex(`${schema}.${table}`).insert([
-    {
-      asset_provider_id: assetProvidersMap["Chase Bank (UK)"],
-      name: "Current Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Chase Bank (UK)"],
-      name: "Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Bank of China (HK)"],
-      name: "HKD Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Bank of China (HK)"],
-      name: "Foreign Currency Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Bank of China (HK)"],
-      name: "HKD Current Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Bank of China (HK)"],
-      name: "USD Current Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["HSBC (HK)"],
-      name: "HKD Current Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["HSBC (HK)"],
-      name: "HKD Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["HSBC (UK)"],
-      name: "Current Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["HSBC (UK)"],
-      name: "Online Bonus Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["HSBC (UK)"],
-      name: "Regular Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["HSBC (UK)"],
-      name: "Flexible Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["HSBC (UK)"],
-      name: "Fixed Rate Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["HSBC (UK)"],
-      name: "Loyalty Cash ISA",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Lloyds Bank (UK)"],
-      name: "Classic Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Lloyds Bank (UK)"],
-      name: "Easy Saver",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Lloyds Bank (UK)"],
-      name: "Cash ISA Saver",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Lloyds Bank (UK)"],
-      name: "1 Year Fixed Rate Cash ISA",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Citibank (HK)"],
-      name: "Checking Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Citibank (HK)"],
-      name: "HKD Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Citibank (HK)"],
-      name: "USD Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Citibank (HK)"],
-      name: "GBP Global Wallet",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Hang Seng Bank (HK)"],
-      name: "HKD Savings / Current Wallet",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Metro Bank (UK)"],
-      name: "Current Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Metro Bank (UK)"],
-      name: "Instant Access Savings",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Metro Bank (UK)"],
-      name: "Instant Access Cash ISA",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Metro Bank (UK)"],
-      name: "Fixed Rate Cash ISA",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Metro Bank (UK)"],
-      name: "Fixed Term Savings",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Monzo (UK)"],
-      name: "Current Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Monzo (UK)"],
-      name: "Instant Access Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Monzo (UK)"],
-      name: "Easy Access Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Monzo (UK)"],
-      name: "Easy Access ISA Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Monzo (UK)"],
-      name: "Fixed Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Nationwide (UK)"],
-      name: "FlexPlus",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Nationwide (UK)"],
-      name: "FlexDirect",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Nationwide (UK)"],
-      name: "FlexAccount",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Nationwide (UK)"],
-      name: "1 Year Fixed Rate ISA",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Nationwide (UK)"],
-      name: "1 Year Triple Access Online ISA",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Nationwide (UK)"],
-      name: "Flex Instant Saver",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Nationwide (UK)"],
-      name: "Instant Access Saver",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Starling (UK)"],
-      name: "Current Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Standard Chartered (HK)"],
-      name: "HKD Current Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Standard Chartered (HK)"],
-      name: "Payroll Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Standard Chartered (HK)"],
-      name: "HKD Marathon Savings Account",
-    },
-    {
-      asset_provider_id: assetProvidersMap["Standard Chartered (HK)"],
-      name: "USD Marathon Savings Account",
-    },
-  ]);
+    const assetProviders: { id: string; name: string }[] = await knex(
+      `${schema}.asset_provider`
+    ).select("id", "name");
+    const assetProvidersMap: any = {};
+    assetProviders.forEach((assetProvider) => {
+      assetProvidersMap[assetProvider.name] = assetProvider.id;
+    });
+
+    await knex(`${schema}.${table}`).insert([
+      {
+        asset_provider_id: assetProvidersMap["Chase Bank (UK)"],
+        name: "Current Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Chase Bank (UK)"],
+        name: "Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Bank of China (HK)"],
+        name: "HKD Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Bank of China (HK)"],
+        name: "Foreign Currency Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Bank of China (HK)"],
+        name: "HKD Current Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Bank of China (HK)"],
+        name: "USD Current Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["HSBC (HK)"],
+        name: "HKD Current Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["HSBC (HK)"],
+        name: "HKD Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["HSBC (UK)"],
+        name: "Current Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["HSBC (UK)"],
+        name: "Online Bonus Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["HSBC (UK)"],
+        name: "Regular Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["HSBC (UK)"],
+        name: "Flexible Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["HSBC (UK)"],
+        name: "Fixed Rate Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["HSBC (UK)"],
+        name: "Loyalty Cash ISA",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Lloyds Bank (UK)"],
+        name: "Classic Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Lloyds Bank (UK)"],
+        name: "Easy Saver",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Lloyds Bank (UK)"],
+        name: "Cash ISA Saver",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Lloyds Bank (UK)"],
+        name: "1 Year Fixed Rate Cash ISA",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Citibank (HK)"],
+        name: "Checking Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Citibank (HK)"],
+        name: "HKD Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Citibank (HK)"],
+        name: "USD Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Citibank (HK)"],
+        name: "GBP Global Wallet",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Hang Seng Bank (HK)"],
+        name: "HKD Savings / Current Wallet",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Metro Bank (UK)"],
+        name: "Current Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Metro Bank (UK)"],
+        name: "Instant Access Savings",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Metro Bank (UK)"],
+        name: "Instant Access Cash ISA",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Metro Bank (UK)"],
+        name: "Fixed Rate Cash ISA",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Metro Bank (UK)"],
+        name: "Fixed Term Savings",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Monzo (UK)"],
+        name: "Current Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Monzo (UK)"],
+        name: "Instant Access Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Monzo (UK)"],
+        name: "Easy Access Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Monzo (UK)"],
+        name: "Easy Access ISA Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Monzo (UK)"],
+        name: "Fixed Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Nationwide (UK)"],
+        name: "FlexPlus",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Nationwide (UK)"],
+        name: "FlexDirect",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Nationwide (UK)"],
+        name: "FlexAccount",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Nationwide (UK)"],
+        name: "1 Year Fixed Rate ISA",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Nationwide (UK)"],
+        name: "1 Year Triple Access Online ISA",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Nationwide (UK)"],
+        name: "Flex Instant Saver",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Nationwide (UK)"],
+        name: "Instant Access Saver",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Starling (UK)"],
+        name: "Current Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Standard Chartered (HK)"],
+        name: "HKD Current Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Standard Chartered (HK)"],
+        name: "Payroll Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Standard Chartered (HK)"],
+        name: "HKD Marathon Savings Account",
+      },
+      {
+        asset_provider_id: assetProvidersMap["Standard Chartered (HK)"],
+        name: "USD Marathon Savings Account",
+      },
+    ]);
+  }
 }
